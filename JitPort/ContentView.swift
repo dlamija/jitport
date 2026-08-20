5bb4a7eb32f7cdb92c7b1392cb4c822c4a05b5c0
dlamija <mija@ramiza.com>
2026-08-20 11:20:50 +0800

    Initial Commit

---
+++ JitPort/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JitPort
+//
+//  Created by Muhammed Ramiza on 20/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JitPort/JitPortApp.swift
@@ -0,0 +1,17 @@
+//
+//  JitPortApp.swift
+//  JitPort
+//
+//  Created by Muhammed Ramiza on 20/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct JitPortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
